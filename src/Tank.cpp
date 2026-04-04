5f33d4904b4529a8709d94b721b21c334e369e18
aset <rahmgalievaset@gmail.com>
2026-04-04 13:20:02 +0500

    fixed

--- src/Tank.cpp
+++ src/Tank.cpp
@@ -1,4 +1,5 @@
 #include "Tank.h"
+#include "AFMotor.h"
 #include <math.h>
 
 
@@ -27,8 +28,8 @@ struct Vec2 {
 
 
 Vec2 vector;
-uint8_t left;
-uint8_t right;
+int16_t left;
+int16_t right;
 Tank::Tank(float dist)
 : left_motor(AF_DCMotor(1)), right_motor(AF_DCMotor(2)), half_dist(dist/2.0) {}
 
@@ -39,4 +40,14 @@ void Tank::move(float x, float y) {
 
     left = vector.y - (vector.x * half_dist);
     right = vector.y + (vector.x * half_dist);
+
+    if (left < 0) left_motor.run(BACKWARD);
+    else if (left == 0) left_motor.run(RELEASE);
+    else if (left > 0) left_motor.run(FORWARD);
+    left_motor.setSpeed( (left < 0) ? -left : left );
+
+    if (right < 0) right_motor.run(BACKWARD);
+    else if (right == 0) right_motor.run(RELEASE);
+    else if (right > 0) right_motor.run(FORWARD);
+    right_motor.setSpeed( (right < 0) ? -right : right );
 }
